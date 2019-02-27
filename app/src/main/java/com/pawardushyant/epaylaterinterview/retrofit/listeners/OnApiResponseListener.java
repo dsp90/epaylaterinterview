809880f5e2ca9a5a7cb3089422a4a3ec6d33720a
dushyant <pawardushyant22@gmail.com>
2019-02-28 01:07:06 +0530

    refactor

--- app/src/main/java/com/pawardushyant/epaylaterinterview/retrofit/listeners/OnApiResponseListener.java
+++
@@ -1,7 +0,0 @@
-package com.pawardushyant.epaylaterinterview.retrofit.listeners;
-
-import retrofit2.Response;
-
-public interface OnApiResponseListener<T> {
-    void onApiServiceResponse(T response);
-}
